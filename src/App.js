07ef063bb565fef14e21d9cc584798b61fd49465
alezama <alezama@hushmail.me>
2024-01-24 19:53:45 -0600

    create pages and routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,43 @@
 import './App.css';
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import Home from './pages/Home'
+import Profile from "./pages/Profile";
+import SignUp from "./pages/SignUp";
+import SignIn from "./pages/SignIn";
+import ForgotPassword from "./pages/ForgotPassword";
+import Offers from "./pages/Offers";
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Home/>,
+    },
+    {
+        path: 'profile',
+        element: <Profile/>,
+    },
+    {
+        path: 'sign-up',
+        element: <SignUp/>,
+    },
+    {
+        path: 'sign-in',
+        element: <SignIn/>,
+    },
+    {
+        path: 'forgot-password',
+        element: <ForgotPassword/>,
+    },
+    {
+        path: 'offers',
+        element: <Offers/>,
+    }
+
+])
 
 function App() {
   return (
-   <>
-     <h1>Hello World!</h1>
-   </>
+   <RouterProvider router={router} />
   );
 }
 
